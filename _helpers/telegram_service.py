f22194717c9a1b82b6dc709e9886fb7d5c9db70a
Amin_Seifi <aminseiifi@gmail.com>
2022-06-18 00:55:28 +0430

    Delete Async for all telegram Messages.

--- _helpers/telegram_service.py
+++ _helpers/telegram_service.py
@@ -6,49 +6,44 @@ from django.conf import settings
 class InternalService:
 
     @staticmethod
-    async def _send_message(context, message, to):
-        result = await context.bot.send_message(chat_id=to,
-                                                text=message,
-                                                parse_mode=ParseMode.MARKDOWN)
+    def _send_message(context, message, to):
+        result = context.bot.send_message(chat_id=to,
+                                          text=message,
+                                          parse_mode=ParseMode.MARKDOWN)
         return result.message_id
 
     @staticmethod
-    async def _send_file(context, file, filename, thumb, caption):
-        result = await context.bot.send_document(chat_id=TELEGRAM_FILES_CHANNEL,
-                                                 document=file,
-                                                 filename=filename,
-                                                 thumb=thumb,
-                                                 caption=caption,
-                                                 parse_mode=ParseMode.MARKDOWN)
+    def _send_file(context, file, filename, thumb, caption):
+        result = context.bot.send_document(chat_id=TELEGRAM_FILES_CHANNEL,
+                                           document=file,
+                                           filename=filename,
+                                           thumb=thumb,
+                                           caption=caption,
+                                           parse_mode=ParseMode.MARKDOWN)
         return result.message_id
 
     @classmethod
-    async def send_info(cls, context, info):
-        result = await cls._send_message(context=context,
-                                         message=settings.TELEGRAM_MESSAGES['info'].format(info=str(info)),
-                                         to=TELEGRAM_INFO_GROUP)
-        return result
+    def send_info(cls, context, info):
+        return cls._send_message(context=context,
+                                 message=settings.TELEGRAM_MESSAGES['info'].format(info=str(info)),
+                                 to=TELEGRAM_INFO_GROUP)
 
     @classmethod
-    async def send_warning(cls, context, warning):
-        result = await cls._send_message(context=context,
-                                         message=settings.TELEGRAM_MESSAGES['warning'].format(warning=str(warning)),
-                                         to=TELEGRAM_WARNING_GROUP)
-        return result
+    def send_warning(cls, context, warning):
+        return cls._send_message(context=context,
+                                 message=settings.TELEGRAM_MESSAGES['warning'].format(warning=str(warning)),
+                                 to=TELEGRAM_WARNING_GROUP)
 
     @classmethod
-    async def send_error(cls, context, error):
-        result = await cls._send_message(context=context,
-                                         message=settings.TELEGRAM_MESSAGES['error'].format(ex=str(error)),
-                                         to=TELEGRAM_ERROR_GROUP)
-        return result
+    def send_error(cls, context, error):
+        return cls._send_message(context=context,
+                                 message=settings.TELEGRAM_MESSAGES['error'].format(ex=str(error)),
+                                 to=TELEGRAM_ERROR_GROUP)
 
     @classmethod
     async def send_file(cls, context, file, filename, thumb, description):
-        result = await cls._send_file(context=context,
-                                      file=file,
-                                      filename=filename,
-                                      thumb=thumb,
-                                      caption=description)
-
-        return result
+        return cls._send_file(context=context,
+                              file=file,
+                              filename=filename,
+                              thumb=thumb,
+                              caption=description)
