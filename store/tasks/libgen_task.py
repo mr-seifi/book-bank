8ce3a2ff855c1bd9ae9ae2a1e1a1814f09f688e0
Amin_Seifi <aminseiifi@gmail.com>
2022-06-17 20:20:46 +0430

    Log downloads.

--- store/tasks/libgen_task.py
+++ store/tasks/libgen_task.py
@@ -111,7 +111,7 @@ def _download_book(book: Book, session: requests.Session, context):
     print(f'[+] Download {book.title} started!')
     content = ContentFile(session.get(book.download_url).content)
     filename = f'{LibgenService.get_book_identifier(book.__dict__)}.{book.extension}'
-    message_id = InternalService.send_file(context=context, file=content, filename=filename,
+    message_id = InternalService.send_file(context=context, file=content.file, filename=filename,
                                            thumb=book.cover, description=f'*{book.title}*\n{book.description}'[:500]
                                                                          + f'...\n\n#{book.topic}')
     book.file = message_id
