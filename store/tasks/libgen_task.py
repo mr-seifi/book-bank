e95ad7b7b5ab5312ccdd6dfad1e4eee0edb17c4b
Amin_Seifi <aminseiifi@gmail.com>
2022-06-18 00:50:37 +0430

    Use Async for all telegram Messages.

--- store/tasks/libgen_task.py
+++ store/tasks/libgen_task.py
@@ -110,7 +110,7 @@ async def _download_book(book: Book, session, context):
 
     print(f'[+] Download {book.title} started!')
     result = await session.get(book.download_url)
-    content = result.content
+    content = await result.read()
     filename = f'{LibgenService.get_book_identifier(book.__dict__)}.{book.extension}'
     message_id = await InternalService.send_file(context=context, file=content, filename=filename,
                                                  thumb=book.cover,
