ee7e17cadb758b18d699634ccb6b309467b014b2
Amin_Seifi <aminseiifi@gmail.com>
2022-06-17 18:19:18 +0430

    Forward message.

--- _helpers/telegram_service.py
+++ _helpers/telegram_service.py
@@ -1,4 +1,4 @@
-from secret import TELEGRAM_INFO_GROUP, TELEGRAM_WARNING_GROUP, TELEGRAM_ERROR_GROUP
+from secret import TELEGRAM_INFO_GROUP, TELEGRAM_WARNING_GROUP, TELEGRAM_ERROR_GROUP, TELEGRAM_FILES_CHANNEL
 from telegram import ParseMode
 from django.conf import settings
 
@@ -7,24 +7,38 @@ class InternalService:
 
     @staticmethod
     def _send_message(context, message, to):
-        print(context.bot.send_message(chat_id=to,
-                                       text=message,
-                                       parse_mode=ParseMode.MARKDOWN))
+        result = context.bot.send_message(chat_id=to,
+                                          text=message,
+                                          parse_mode=ParseMode.MARKDOWN)
+        return result.get('message_id')
 
     @classmethod
     def send_info(cls, context, info):
-        cls._send_message(context=context,
-                          message=settings.TELEGRAM_MESSAGES['info'].format(info=str(info)),
-                          to=TELEGRAM_INFO_GROUP)
+        return cls._send_message(context=context,
+                                 message=settings.TELEGRAM_MESSAGES['info'].format(info=str(info)),
+                                 to=TELEGRAM_INFO_GROUP)
 
     @classmethod
     def send_warning(cls, context, warning):
-        cls._send_message(context=context,
-                          message=settings.TELEGRAM_MESSAGES['warning'].format(warning=str(warning)),
-                          to=TELEGRAM_WARNING_GROUP)
+        return cls._send_message(context=context,
+                                 message=settings.TELEGRAM_MESSAGES['warning'].format(warning=str(warning)),
+                                 to=TELEGRAM_WARNING_GROUP)
 
     @classmethod
     def send_error(cls, context, error):
-        cls._send_message(context=context,
-                          message=settings.TELEGRAM_MESSAGES['error'].format(ex=str(error)),
-                          to=TELEGRAM_ERROR_GROUP)
+        return cls._send_message(context=context,
+                                 message=settings.TELEGRAM_MESSAGES['error'].format(ex=str(error)),
+                                 to=TELEGRAM_ERROR_GROUP)
+
+    @staticmethod
+    def _send_file(context, file, filename):
+        result = context.bot.send_document(chat_id=TELEGRAM_FILES_CHANNEL,
+                                           document=file,
+                                           filename=filename)
+        return result.get('message_id')
+
+    @classmethod
+    def send_file(cls, context, file, filename):
+        return cls._send_file(context=context,
+                              file=file,
+                              filename=filename)
