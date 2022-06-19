13b38ce0182553863919f821d3f5bb3bd8d447f5
Amin_Seifi <aminseiifi@gmail.com>
2022-06-20 00:22:04 +0430

    Remove async from download

--- store/tasks/libgen_task.py
+++ store/tasks/libgen_task.py
@@ -120,7 +120,9 @@ async def _download_book(book: Book, session, context, bulk=False):
     filename = f'{LibgenService.get_book_identifier(book.__dict__)}.{book.extension}'
 
     if not book.cover:
-        _download_cover(session, book)
+        cover_name, cover = await LibgenService.download_cover(book, session)
+        book.cover.save(name=cover_name, content=cover)
+    print(book.cover)
     message_id = InternalService.send_file(context=context, file=content, filename=filename,
                                            thumb=book.cover,
                                            description=f'*{book.title}*\n{book.description}'[:500]
