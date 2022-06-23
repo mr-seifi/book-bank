889e6d51601cab3ddec1c8b4b0a8fb6ca56cb319
Amin_Seifi <aminseiifi@gmail.com>
2022-06-23 14:11:10 +0430

    Migrations

--- provider/models.py
+++ provider/models.py
@@ -3,8 +3,6 @@ from .services.zlib_service import ZLibCache
 
 
 class ZlibAccount(models.Model):
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=50)
     user_id = models.IntegerField()
     user_key = models.CharField(max_length=50)
 
