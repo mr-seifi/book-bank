ebc0c3d50a41bc68dba24c924664438007d5e02d
Amin_Seifi <aminseiifi@gmail.com>
2022-06-26 22:30:35 +0430

    Write DownloadBookRedirector.

--- provider/views.py
+++ provider/views.py
@@ -1,6 +1,5 @@
 from django.views.generic import RedirectView
 from store.models import Book
-from rest_framework.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 
 
@@ -11,10 +10,10 @@ class BookDownloadUrlRedirectView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         md5 = kwargs.get('md5')
         if not md5:
-            ValidationError('Are you kidding me?')
+            return
 
         book = get_object_or_404(Book, md5=md5)
         if book.filesize < self.DOWNLOAD_LIMIT_SIZE:
-            return ValidationError('Are you kidding me?')
+            return
 
         return book.download_url
