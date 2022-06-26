e218e44904c73681b294b1348471ea4c690bcef5
Amin_Seifi <aminseiifi@gmail.com>
2022-06-26 22:25:56 +0430

    Write DownloadBookRedirector.

--- book_bank/settings/base.py
+++ book_bank/settings/base.py
@@ -19,7 +19,6 @@ from ..celery import app
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -31,7 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'store.apps.StoreConfig',
     'provider.apps.ProviderConfig',
     'monitoring.apps.MonitoringConfig',
@@ -95,7 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -106,7 +104,6 @@ TIME_ZONE = 'Asia/Tehran'
 USE_I18N = True
 
 USE_TZ = False
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -150,4 +147,3 @@ REDIS_HOST = '127.0.0.1'
 REDIS_PORT = '6379'
 
 RELEASE_DATE = make_aware(datetime.datetime.strptime('2022-06-20 00:43:40', '%Y-%m-%d %H:%M:%S'))
-
--- provider/views.py
+++ provider/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import RedirectView
+from store.models import Book
+from rest_framework.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class BookDownloadUrlRedirectView(RedirectView):
+
+    DOWNLOAD_LIMIT_SIZE = int(7e7)
+
+    def get_redirect_url(self, *args, **kwargs):
+        md5 = kwargs.get('md5')
+        if not md5:
+            ValidationError('Are you kidding me?')
+
+        book = get_object_or_404(Book, md5=md5)
+        if book.filesize < self.DOWNLOAD_LIMIT_SIZE:
+            return ValidationError('Are you kidding me?')
+
+        return book.download_url
