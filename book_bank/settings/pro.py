06842e4e46e27b433f00780774b867e00861a85b
Mr.Seifi <work.aseifi@gmail.com>
2022-06-26 12:44:49 +0430

    uWSGI configs.

--- book_bank/settings/pro.py
+++ book_bank/settings/pro.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+ADMINS = (
+    ('Amin Seifi', 'aminseiifi@gmail.com'),
+)
+
+ALLOWED_HOSTS = ['*']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
