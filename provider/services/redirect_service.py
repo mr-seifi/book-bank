a92bdde6ce04ead0b4dd7be3d34890c0ffd9efc3
Amin_Seifi <aminseiifi@gmail.com>
2022-06-29 00:00:17 +0430

    Write new url generation.

--- provider/services/redirect_service.py
+++ provider/services/redirect_service.py
@@ -3,4 +3,8 @@ class RedirectService:
     BASE_URL = 'https://book-bank.net/download'
 
     def generate_redirect_url(self, book):
-        return f'{self.BASE_URL}/{book.md5}/'
+        if not book.download_url:
+            return
+
+        second_part_url = book.download_url.split('main/')[1]
+        return f'{self.BASE_URL}/book/{second_part_url}'
