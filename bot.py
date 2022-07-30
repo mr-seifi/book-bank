4e0e9a8075ab5e6adcd0f08a6dd62e212944c08c
Amin_Seifi <aminseiifi@gmail.com>
2022-07-30 16:28:17 +0430

    Edit start command.

--- bot.py
+++ bot.py
@@ -104,86 +104,6 @@ class Main:
 
         return 1
 
-    @staticmethod
-    async def download_inline(update: Update, context: CallbackContext):
-        from uuid import uuid4
-
-        query = update.inline_query.query
-        user = update.inline_query.from_user
-
-        if query == "":
-            return
-
-        results = [
-            InlineQueryResultArticle(
-                id=str(uuid4()),
-                title=book.title,
-                input_message_content=InputTextMessageContent(f'/download {book.md5}'),
-                thumb_url=book.cover_url,
-                description=f'{book.year}-{book.extension}-{book.filesize // 1000000}MB\n'
-                            f'{book.authors}\n{book.publisher}\n{book.description}'
-            ) for book in Book.objects.filter(document__exact=query).exclude(title__exact='').order_by('document')[:25]
-        ]
-        response = await update.inline_query.answer(results)
-
-        await InternalService.send_info(context=context, info=f'[{user.full_name}](tg://user?id={user.id}) is searching'
-                                                              f' for {query}! \U0001F60F')
-        return response
-
-    @staticmethod
-    async def download(update: Update, context: CallbackContext):
-        message = update.message
-        user_id = message.from_user.id
-        user = User.objects.get(user_id=user_id)
-
-        verified = await Main.check_verification(message=message)
-        if not verified and not user.plan:
-            return
-
-        account_service = AccountCacheService()
-        limit = account_service.get_limit(user_id=user_id)
-        if limit > settings.USER_DOWNLOAD_LIMIT and not user.plan:
-            await message.reply_text(
-                settings.TELEGRAM_MESSAGES['limited_download']
-            )
-            return
-
-        md5 = context.args[0]
-
-        await message.reply_text(
-            settings.TELEGRAM_MESSAGES['waiting_for_download']
-        )
-
-        book = Book.objects.get(md5=md5)
-
-        if book.file:
-            message_id = book.file
-            await InternalService.send_info(context,
-                                            f'[{user.fullname}](tg://user?id={user.user_id}) is getting {book.title}'
-                                            f' from forwarding\.')
-            await InternalService.forward_file(context=context,
-                                               file_id=message_id,
-                                               to=user_id)
-        elif book.filesize >= settings.DOWNLOAD_LIMIT_SIZE:
-            await InternalService.send_info(context,
-                                            f'[{user.fullname}](tg://user?id={user.user_id}) is getting {book.title}'
-                                            f' from link\.')
-            await message.reply_text(
-                settings.TELEGRAM_MESSAGES['redirect_url'].format(title=book.title[:100],
-                                                                  year=book.year,
-                                                                  extension=book.extension,
-                                                                  authors=book.authors[:50],
-                                                                  publisher=book.publisher[:50],
-                                                                  description=book.description[:250],
-                                                                  url=RedirectService().generate_redirect_url(book)),
-                parse_mode=ParseMode.MARKDOWN
-            )
-
-        else:
-            asyncio.create_task(download_book(book, context=context, user=message.from_user))
-
-        account_service.incr_limit(user_id=user_id)
-
 
 class Payment:
 
@@ -350,14 +270,84 @@ class Payment:
 class Search:
 
     @staticmethod
-    async def search(update: Update, context: CallbackContext):
-        query = update.callback_query
-        # user_id = query.from_user.id
-        await query.answer()
-        # query.message.reply_text(
-        #     swit
-        # )
-        # await update.
+    async def download_inline(update: Update, context: CallbackContext):
+        from uuid import uuid4
+
+        query = update.inline_query.query
+        user = update.inline_query.from_user
+
+        if query == "":
+            return
+
+        results = [
+            InlineQueryResultArticle(
+                id=str(uuid4()),
+                title=book.title,
+                input_message_content=InputTextMessageContent(f'/download {book.md5}'),
+                thumb_url=book.cover_url,
+                description=f'{book.year}-{book.extension}-{book.filesize // 1000000}MB\n'
+                            f'{book.authors}\n{book.publisher}\n{book.description}'
+            ) for book in Book.objects.filter(document__exact=query).exclude(title__exact='').order_by('document')[:25]
+        ]
+        response = await update.inline_query.answer(results)
+
+        await InternalService.send_info(context=context, info=f'[{user.full_name}](tg://user?id={user.id}) is searching'
+                                                              f' for {query}! \U0001F60F')
+        return response
+
+    @staticmethod
+    async def download(update: Update, context: CallbackContext):
+        message = update.message
+        user_id = message.from_user.id
+        user = User.objects.get(user_id=user_id)
+
+        verified = await Main.check_verification(message=message)
+        if not verified and not user.plan:
+            return
+
+        account_service = AccountCacheService()
+        limit = account_service.get_limit(user_id=user_id)
+        if limit > settings.USER_DOWNLOAD_LIMIT and not user.plan:
+            await message.reply_text(
+                settings.TELEGRAM_MESSAGES['limited_download']
+            )
+            return
+
+        md5 = context.args[0]
+
+        await message.reply_text(
+            settings.TELEGRAM_MESSAGES['waiting_for_download']
+        )
+
+        book = Book.objects.get(md5=md5)
+
+        if book.file:
+            message_id = book.file
+            await InternalService.send_info(context,
+                                            f'[{user.fullname}](tg://user?id={user.user_id}) is getting {book.title}'
+                                            f' from forwarding\.')
+            await InternalService.forward_file(context=context,
+                                               file_id=message_id,
+                                               to=user_id)
+        elif book.filesize >= settings.DOWNLOAD_LIMIT_SIZE:
+            await InternalService.send_info(context,
+                                            f'[{user.fullname}](tg://user?id={user.user_id}) is getting {book.title}'
+                                            f' from link\.')
+            await message.reply_text(
+                settings.TELEGRAM_MESSAGES['redirect_url'].format(title=book.title[:100],
+                                                                  year=book.year,
+                                                                  extension=book.extension,
+                                                                  authors=book.authors[:50],
+                                                                  publisher=book.publisher[:50],
+                                                                  description=book.description[:250],
+                                                                  url=RedirectService().generate_redirect_url(book)),
+                parse_mode=ParseMode.MARKDOWN
+            )
+
+        else:
+            asyncio.create_task(download_book(book, context=context, user=message.from_user))
+
+        account_service.incr_limit(user_id=user_id)
 
 
 def main():
@@ -369,7 +359,6 @@ def main():
         states={
             settings.STATES['start']: [
                 CallbackQueryHandler(Payment.payment, pattern=r'^PAYMENT$'),
-                CallbackQueryHandler(Search.search, pattern=r'^SEARCH$'),
             ],
             settings.STATES['payment']: [
                 CallbackQueryHandler(Payment.plan_selection, pattern=r'^cryptocurrency$'),
@@ -387,12 +376,12 @@ def main():
         },
         fallbacks=[CommandHandler('start', Main.start)]
     )
-    download_handler = CommandHandler('download', Main.download)
+    download_handler = CommandHandler('download', Search.download)
 
     # application.add_handler(start_handler)
     application.add_handler(menu_handler)
     application.add_handler(download_handler)
-    application.add_handler(InlineQueryHandler(Main.download_inline))
+    application.add_handler(InlineQueryHandler(Search.download_inline))
 
     # Start the Bot
     application.run_polling()
